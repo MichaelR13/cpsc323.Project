639c33ecc672e640348267f89ac34da1ee017ffd
MichaelR13 <michaels.rojas13@csu.fullerton.edu>
2023-03-07 00:28:23 -0800

    Lexer is working, only need I/O

--- a1.lexer/FILES/lexer.cpp
+++ a1.lexer/FILES/lexer.cpp
@@ -17,6 +17,8 @@
 
 using namespace std;
 
+ifstream fin("input.txt");
+ofstream fout("output.txt");
 // function that asks the user for a file name and opens the file
 void openFiles()
 {
@@ -27,16 +29,32 @@ void openFiles()
     ofstream fout("output.txt");
 }
 
+void initPrint()// This will print the header for the output file
+{
+    fout << "token___________lexeme" << endl;
+}
+
+// Check if a value in the array matches
+bool checkArr(char value, char* array, int size)
+{
+    for (int counter = 0; counter < size; counter++)
+    {
+        if (value == array[counter])
+        {
+            return true;
+        }
+    }
+}
+
 // FSM for integers and real numbers
 string intRealFSM(char num) {
 
-    // There will be 4 states: 1 - Integer, 2 - Real, 3 - Error, 4 - End
+    // There will be 3 states: 1 - Integer, 2 - Real, 3 - Error
     int fsmIntReal[5][3] = {
         /*0*/               {1, 3, 3},
         /*1*/               {1, 2, 3},
         /*2*/               {2, 3, 3},
-        /*3*/               {3, 3, 3},
-        /*4*/               {4, 4, 4}
+        /*3*/               {3, 3, 3}
     };
 
     // Track position in FSM
@@ -72,12 +90,6 @@ string intRealFSM(char num) {
             break;
         }
 
-        else if (pos == 4)
-        {
-            arr[i] = '\0';
-            break;
-        }
-
         else
         {
             arr[i++] = charPlusOne;
@@ -88,132 +100,58 @@ string intRealFSM(char num) {
     return arr;
 }
 
-string identifierFSM(char letter) {
-    // There will be 4 states: 1 - Identifier, 2 - Error, 3 - End
-    int fsmIdentifier[4][2] = {
-        /*0*/               {1, 2},
-        /*1*/               {1, 3},
-        /*2*/               {2, 2},
-        /*3*/               {3, 3}
-    };
-
-    // Track position in FSM
-    int pos = 1;
-
-    // Hold the letters in an array
-    char arr[100];
-    arr[0] = letter;    // Put the first letter in the array
-    int i = 1;
-    char charPlusOne;
-
-    // Loop through the file until the end of the file
-    while (charPlusOne = fin.peek())    // fin.peek() will preview the next character in the file 
-    {
-        if (isalpha(charPlusOne))
-        {
-            pos = fsmIdentifier[pos][0];
-        }
-
-        else if (isdigit(charPlusOne))
-        {
-            pos = fsmIdentifier[pos][0];
-        }
-
-        else
-        {
-            pos = fsmIdentifier[pos][1];
-        }
-
-        if (pos == 2)
-        {
-            arr[i] = '\0';
-            break;
-        }
-
-        else if (pos == 3)
-        {
-            arr[i] = '\0';
-            break;
-        }
-
-        else
-        {
-            arr[i++] = charPlusOne;
-        }
-
-        fin.get();  // fin.get() will get the next character in the file
-    }
-    return  arr;
-}
-
-string isKeyword(char arr[])    // this function will check if the identifier is a keyword
+string isKeyword(char buf[])    // this function will check if the identifier is a keyword
 {
     string keywords[11] = { "if", "then", "else", "end", "repeat", "until", "read", "write", "while", "do", "endwhile" };
-    int i = 0;  // counter
+    int counter = 0;  // counter
     
-    for (i = 0; i < 11; i++)
+    for (counter = 0; counter < 11; ++counter)
     {
-        if (strcmp(arr, keywords[i].c_str()) == 0)
+        if (strcmp(keywords[counter].c_str(), buf) == 0)
         {
-            return keywords[i];
+            return keywords[counter];
         }
     }
-    return "Not a keyword";
-}
-
-// Check if a value in the array matches
-bool checkArr(char value, char* array, int size)
-{
-    for (int instance = 0; instance < size; instance++)
-    {
-        if (value == array[instance])
-        {
-            return true;
-        }
-    }
+    return "-1";
 }
 
 void lexer()
 {
-
-    
-    char character;
+    char character; // This will hold the character that is being read from the file
 
     // Make sure the file is open
     if (!fin.is_open())
     {
         cout << "Error opening file" << endl;
-        exit(1);
+        exit(0);
     }
 
     // Loop through the file until the end of the file
     while (!fin.eof())
     {
         character = fin.get();  // fin.get() will get the next character in the file
-        char ops[11] = { '+', '-', '*', '/', '=', '<', '>', '!', '&', '|', '%' };
-        char seps[10] = { '(', ')', '{', '}', '[', ']', ',', ';', ':', '#' };
+        char ops[10] = { '+', '-', '*', '/', '=', '<', '!', '&', '|', '%' };
+        char seps[12] = { '%', '(', ')', '{', '}', '[', ']', ',', ';', ':', '#', '*' };
 
         // Check if the character is a letter
         if (isalpha(character))
         {
-            int fsmIntReal[5][3] = {
+            int fsmIntReal[4][3] = {
                 /*0*/               {1, 3, 3},
                 /*1*/               {1, 2, 3},
                 /*2*/               {2, 3, 3},
                 /*3*/               {3, 3, 3},
-                /*4*/               {4, 4, 4}
             };
 
             int i = 0;  // counter
             char IDarr[100];
             IDarr[i] = character;
-            i++;
             char nextChar = fin.peek(); // fin.peek() will preview the next character in the file
 
             while (isalnum(nextChar) || nextChar == '_')
             {
-                IDarr[i] = nextChar;
                 i++;
+                IDarr[i] = nextChar;
                 fin.get();  // fin.get() will get the next character in the file
                 nextChar = fin.peek();  // fin.peek() will preview the next character in the file
             }
@@ -222,69 +160,119 @@ void lexer()
 
             string kWord = isKeyword(IDarr);
             
-            if (kWord != "Not a keyword")
+            if (kWord != "-1")
             {
-                cout << kWord << " is a keyword" << endl;
-                fout << kWord << " is a keyword" << endl;
+				cout << kWord << "\t\t\tkeyword\n";
+				fout << kWord << "\t\t\tkeyword\n";
             }
             else
             {
-                cout << IDarr << " is an identifier" << endl;
-                fout << IDarr << " is an identifier" << endl;
+				cout << IDarr << "\t\t\tidentifier\n";
+				fout << IDarr << "\t\t\tidentifier\n";
             }
         }
 
         // Check if the character is a digit
         else if (isdigit(character))
         {
-            string num = intRealFSM(character);
+            string num = intRealFSM(character); // This will hold the number that is being read from the file
 
-            if (num.find(".") != -1)
+			if (num.find(".") != -1) 
             {
-                cout << num << " is a real number" << endl;
-                fout << num << " is a real number" << endl;
+				cout << num << "\t\t\treal\n";
+				fout << num << "\t\t\treal\n";
             }
-            else 
+            else
             {
-                cout << num << " is an integer" << endl;
-                fout << num << " is an integer" << endl;
+				cout << num << "\t\t\tinteger\n";
+				fout << num << "\t\t\tinteger\n";
             }
         }
 
         //Check if it is an operator
-        else if (checkArr(character, ops, 11))
+        else if (checkArr(character, seps, 10))
         {
             char nextChar = fin.peek(); // fin.peek() will preview the next character in the file
-
-            if (nextChar == '=')
+			
+            if (nextChar == '%')
             {
-                cout << character << nextChar << " is an operator" << endl;
-                fout << character << nextChar << " is an operator" << endl;
-                fin.get();  // fin.get() will get the next character in the file
+				cout << character << nextChar << "\t\t\tseperator\n";
+				fout << character << nextChar << "\t\t\tseperator\n";
+				fin.get();
+			}
+			else if (nextChar == ']')
+            { 
+				cout << character << nextChar << "\t\t\tseperator\n";
+				fout << character << nextChar << "\t\t\tseperator\n";
+				fin.get();
+			}
+			else if (nextChar == '*')
+            { 
+				cout << character << nextChar << "\t\t\tseperator\n";
+				fout << character << nextChar << "\t\t\tseperator\n";
+				fin.get();
+				char seeker;
+				while(seeker != '*'){
+					fin.get(seeker);
+				}
+				fin.unget();
+			}
+			else if (character == '*')
+            {
+				cout << character << "\t\t\toperator\n";
+				fout << character << "\t\t\toperator\n";
+			}
+            // Remove any whitespace
+            else if (character == ' ')
+            {
+                continue;
             }
+            // multiple character operators (<=, >=, ==, !=, &&, ||)
+            else if (character == '<' || character == '>' || character == '=' || character == '!' || character == '&' || character == '|')
+            {
+                if (nextChar == '=')
+                {
+                    cout << character << nextChar << "\t\t\toperator\n";
+                    fout << character << nextChar << "\t\t\toperator\n";
+                    fin.get();  // fin.get() will get the next character in the file
+                }
+                else
+                {
+                    cout << character << "\t\t\toperator\n";
+                    fout << character << "\t\t\toperator\n";
+                }
+            }
+            
             else
             {
-                cout << character << " is an operator" << endl;
-                fout << character << " is an operator" << endl;
+				cout << character << "\t\t\tseperator\n";
+				fout << character << "\t\t\tseperator\n";
             }
         }
 
         // Check if it is a separator
-        else if (checkArr(character, seps, 10))
+        else if (checkArr(character, ops, 12))
         {
             char nextChar = fin.peek(); // fin.peek() will preview the next character in the file
-        
-            if (nextChar == '(')
+
+            if (nextChar == '>')
             {
-                cout << character << nextChar << " is a separator" << endl;
-                fout << character << nextChar << " is a separator" << endl;
+				cout << character << nextChar << "\t\t\toperator\n";
+				fout << character << nextChar << "\t\t\toperator\n";
+                fin.get();  // fin.get() will get the next character in the file
+            }
+            else if (nextChar == '=')
+            {
+				cout << character << nextChar << "\t\t\toperator\n";
+				fout << character << nextChar << "\t\t\toperator\n";
                 fin.get();  // fin.get() will get the next character in the file
             }
             else
             {
-                cout << character << " is a separator" << endl;
-                fout << character << " is a separator" << endl;
+				cout << character << "\t\t\toperator\n";
+				fout << character << "\t\t\toperator\n";
             }
+
         }
     }
 
@@ -295,6 +283,7 @@ void lexer()
 
 int main()
 {
+    initPrint();
     lexer();
     return 0;
 }
